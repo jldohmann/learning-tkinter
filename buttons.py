199356d0d9fc4834a5ad2324cb335cee34e1833b
unknown <jldohmann@utexas.edu>
2020-05-16 22:45:16 -0500

    added red bg on click

--- buttons.py
+++ buttons.py
@@ -3,20 +3,31 @@ from tkinter import *
 root = Tk()
 root.geometry('150x150')
 
+# Event functions
 def onEnter(e):
     myButton['background'] = 'green'
 
 def onLeave(e):
     myButton['background'] = 'SystemButtonFace'
 
+def onRelease(e):
+    myButton['background'] = 'SystemButtonFace'
+
+# Return function
+
 def myClick():
     myLabel = Label(root, text="Look! I clicked a button!")
     myLabel.pack()
 
-myButton = Button(root, text="Click Me!", command=myClick)
-myButton.pack()
+# Button creation and binding
+# Button has red background when clicked
+
+myButton = Button(root, text="Click Me!", activebackground='red', command=myClick)
 
 myButton.bind("<Enter>", onEnter)
 myButton.bind("<Leave>", onLeave)
+myButton.bind("<ButtonRelease-1>", onRelease)
+
+myButton.pack()
 
 root.mainloop()
