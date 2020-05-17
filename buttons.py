b75c1717c3d8c039fb2ecfd3f08bf9305457528a
unknown <jldohmann@utexas.edu>
2020-05-16 21:36:39 -0500

    added color change on hover

--- buttons.py
+++ buttons.py
@@ -1,12 +1,22 @@
 from tkinter import *
 
 root = Tk()
+root.geometry('150x150')
+
+def onEnter(e):
+    myButton['background'] = 'green'
+
+def onLeave(e):
+    myButton['background'] = 'SystemButtonFace'
 
 def myClick():
     myLabel = Label(root, text="Look! I clicked a button!")
     myLabel.pack()
 
-myButton = Button(root, text="Click Me!", command=myClick, fg="blue")
+myButton = Button(root, text="Click Me!", command=myClick)
 myButton.pack()
+
+myButton.bind("<Enter>", onEnter)
+myButton.bind("<Leave>", onLeave)
 
 root.mainloop()
